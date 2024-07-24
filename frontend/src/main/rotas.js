ca3e046346db9c71fa02a79433f07cd9587cae19
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2024-07-24 14:46:01 -0300

    Cadastro e atualização de lançamentos

--- frontend/src/main/rotas.js
+++ frontend/src/main/rotas.js
@@ -3,7 +3,8 @@ import { Route, Switch, HashRouter } from 'react-router-dom'
 import Login from '../views/login'
 import CadastroUsuario from '../views/cadastroUsuario'
 import Home from '../views/home'
-import consultaLancamentos from "../views/lancamentos/consultaLancamentos";
+import ConsultaLancamentos from "../views/lancamentos/consultaLancamentos";
+import CadastroLancamentos from "../views/lancamentos/cadastroLancamentos"
 
 function Rotas(){
     
@@ -13,7 +14,8 @@ function Rotas(){
                 <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastroUsuario} />
-                <Route path="/consulta-lancamentos" component={consultaLancamentos} />
+                <Route path="/consulta-lancamentos" component={ConsultaLancamentos} />
+                <Route path="/cadastro-lancamentos" component={CadastroLancamentos} />
                 
             </Switch>
         </HashRouter>
