8088b2afb6988a298c0e154f20dde008f2bffcd6
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2024-07-25 14:47:29 -0300

    lógica de validação do componente de Cadastro de Usuário

--- frontend/src/app/service/usuarioService.js
+++ frontend/src/app/service/usuarioService.js
@@ -1,4 +1,5 @@
 import ApiService from "../apiservice";
+import erroValidacao from "../exception/erroValidacao";
 
 class UsuarioService extends ApiService{
     constructor(){
@@ -11,6 +12,30 @@ class UsuarioService extends ApiService{
 
     obterSaldoPorUsuario(id){
         return this.getObterSaldo(`/${id}/saldo`)
+    }
+
+    validar(usuario){
+        const erros = []
+
+        if(!usuario.nome){
+            erros.push('O campo Nome é obrigatório')
+        }   
+
+        if(!usuario.email){
+            erros.push('O campo Email é obrigatório')
+        }
+        if(!usuario.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
+            erros.push('Informe um Email válido.')
+        }
+        if(!usuario.senha || !usuario.senhaRepeticao){
+            erros.push('Digite a senha 2x')
+        }else if(usuario.senha !== usuario.senhaRepeticao){
+            erros.push('As senhas não batem')
+        }
+
+        if(erros && erros.length > 0){
+            throw new erroValidacao(erros);
+        }
     }
 
     salvar(usuario){
