86554ceb1e6c4538108d880a56a33a7658a561b0
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2024-07-25 15:14:55 -0300

    Caso de uso de alterar status dos lançamentos

--- frontend/src/views/lancamentos/consultaLancamentos.js
+++ frontend/src/views/lancamentos/consultaLancamentos.js
@@ -99,6 +99,21 @@ class consultaLancamentos extends React.Component{
         this.props.history.push('/cadastro-lancamentos')
     }
 
+    alterarStatus = (lancamento, status) => {
+        this.service
+            .alterarStatus(lancamento.id, status)
+            .then( response => {
+                const lancamentos = this.state.lancamentos;
+                const index = lancamentos.indexOf(lancamento);
+                if(index !== -1){
+                    lancamento['status'] = status;
+                    lancamentos[index] = lancamento
+                    this.setState({lancamento});
+                }
+                messages.mostrarSucesso('Status atualizado com sucesso');
+            } )
+    }
+
     render(){
 
         const meses = this.service.obterListaMeses();
@@ -171,7 +186,9 @@ class consultaLancamentos extends React.Component{
                             <LancamentosTable 
                                 lancamentos={this.state.lancamentos} 
                                 deleteAction={this.abrirConfirmacao} 
-                                editAction={this.editar} />
+                                editAction={this.editar}
+                                alterarStatus={this.alterarStatus}
+                                />
                         </div>
                     </div>
                 </div>
